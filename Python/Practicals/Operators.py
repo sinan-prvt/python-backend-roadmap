65b959c0fe1d4f3fab62b0f39485fd96a07efac9
sinan-prvt <mohamedsinan9400@gmail.com>
2026-04-24 21:22:35 +0530

    Added operators practical questions

---
+++ Python/Practicals/Operators.py
@@ -0,0 +1,106 @@
+##========================================= Operators
+
+
+
+# -------------- Arithmetic Operators
+
+# a = 10
+# b = 5
+
+# print(a + b)  # Addition
+# print(a - b)  # Subtraction
+# print(a * b)  # Multiplication
+# print(a / b)  # Division
+# print(a % b)  # Modulus
+# print(a ** b) # Exponentiation
+# print(a // b) # Floor Division
+
+
+
+# -------------- Comparison Operators
+
+
+# a = 10
+# b = 5
+
+# print(a == b)  # Equal to
+# print(a != b)  # Not equal to
+# print(a > b)   # Greater than
+# print(a < b)   # Less than
+# print(a >= b)  # Greater than or equal to
+# print(a <= b)  # Less than or equal to
+
+
+
+# -------------- Assignment Operators
+
+# a = 10
+# b = 5
+
+# a += b  # Add and assign
+# print(a)
+
+# a -= b  # Subtract and assign
+# print(a)
+
+# a *= b  # Multiply and assign
+# print(a)
+
+# a /= b  # Divide and assign
+# print(a)
+
+# a %= b  # Modulus and assign
+# print(a)
+
+# a **= b # Exponentiation and assign
+# print(a)
+
+# a //= b # Floor division and assign
+# print(a)
+
+
+
+# -------------- Logical Operators  
+
+# age = 25
+# print(age > 18 and age < 30) # Logical AND
+
+# role = "Admin"
+# print(role == "Admin" or role == "Manager") # Logical OR
+
+# is_logged_in = False
+# print(not is_logged_in) # Logical NOT
+
+
+
+# -------------- MemberShip Operators
+
+# name = "Sinan"
+# print("i" in name) # in
+
+# print("k" not in name) # not in
+
+
+
+# -------------- Identity Operators
+
+# num1 = [1, 2]
+# num2 = [1, 2, 3]
+
+# print(num1 is num2)
+
+# print(num1 is not num2)
+
+
+
+# -------------- Bitwise Operators
+
+a = 2
+b = 3
+
+print(a & b)  # Bitwise AND (&)
+print(a | b)  # Bitwise OR (|)
+print(a ^ b)  # Bitwise XOR (^)
+print(~10)    # Bitwise NOT (~)
+print(a << b) # Left Shift (<<)
+print(a >> b) # Right Shift(>>)
