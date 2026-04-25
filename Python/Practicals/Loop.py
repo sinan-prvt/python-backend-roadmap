4e57d1891d63c5bd8a959d00491815a3c47cac57
sinan-prvt <mohamedsinan9400@gmail.com>
2026-04-25 11:08:28 +0530

    Added Loops practical questions

---
+++ Python/Practicals/Loop.py
@@ -0,0 +1,75 @@
+##========================================= Loops
+
+
+
+# -------------- Print numbers 1 to 5
+
+# for i in range(1, 6):
+#     print(i)
+
+
+
+# -------------- Print list items
+
+# fruits = ["apple", "banana", "cherry"]
+
+# for item in fruits:
+#     print(item)
+
+
+
+# -------------- Print characters of string
+
+# x = "Sinan"
+
+# for i in x:
+#     print(i)
+
+
+
+# -------------- Print 1 to 5 using while
+
+# i = 1
+
+# while i <= 5:
+#     print(i)
+#     i += 1 
+
+
+
+# -------------- Stop loop when number is found
+
+# for i in range(1, 10):
+#     if i == 3:
+#         break
+#     print(i)
+
+
+
+# -------------- Skip number 5
+
+# for i in range(10):
+#     if i == 5:
+#         continue
+#     print(i)
+
+
+
+# -------------- Print multiplication table
+
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(f"{i} x {j} = {i * j}")
+    
+
+
+
+# ------------- Loop with else
+
+# for i in range(3):
+#     print(i)
+# else:
+#     print("Loop completed.")
+
+
+
