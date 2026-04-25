7cdec4ac59886087d09ebb0fbe19099d4c6c3769
sinan-prvt <mohamedsinan9400@gmail.com>
2026-04-25 10:47:44 +0530

    Added Conditional Statement practical questions

---
+++ Python/Practicals/ConditionalStatement.py
@@ -0,0 +1,70 @@
+##========================================= Conditional Statements
+
+
+
+# -------------- Check if number is positive --> If statement
+
+# num = 10
+
+# if num > 0:
+#     print("The number is positive.")
+
+
+
+# -------------- Check if number is even or odd --> If-else statement
+
+# num = 4
+
+# if num % 2 == 0:
+#     print("The number is even.")
+# else:
+#     print("The number is odd.")
+
+
+
+# -------------- Grade System --> If-elif-else statement
+
+# score = 45
+
+# if score >= 80:
+#     print("Grade: A")
+# elif score >= 60:
+#     print("Grade: B")
+# elif score >= 40:
+#     print("Grade: C")
+# else:
+#     print("Grade: F")
+
+
+
+# -------------- Nested login check --> Nested if statement
+
+# is_logged_in = True
+# is_admin = False
+
+# if is_logged_in:
+#     if is_admin:
+#         print("Welcome, Admin!")
+#     else:
+#         print("Welcome, User!")
+# else:
+#     print("Please log in to continue.")
+
+
+
+# -------------- Short-hand if
+
+# age = 19
+
+# if age >= 18: print("You are an adult.")
+
+
+
+# -------------- Ternary Operator
+
+# age = 20
+
+# result = "Adult" if age >= 18 else "Minor"
+# print(result)
+
+
