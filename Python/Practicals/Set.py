6e193ad5c88302fa2273c36e93cd15fe4d2757e9
sinan-prvt <mohamedsinan9400@gmail.com>
2026-04-27 16:52:02 +0530

    Added Set Practiical Questions

---
+++ Python/Practicals/Set.py
@@ -0,0 +1,129 @@
+##========================================= Set
+
+
+
+# -------------- Add single value
+
+# x = {1, 2, 3}
+
+# x.add(4)
+# print(x)
+
+
+
+# -------------- Add Duplicate value
+
+# x = {1, 2, 3}
+
+# x.add(2)
+# print(x)
+
+
+
+# -------------- Update multiple values
+
+# x = {1, 2, 3}
+
+# x.update([4, 5, 6])
+# print(x)
+
+
+
+# -------------- Update using another set
+
+# x = {1, 2, 3}
+# y = {4, 5, 6}
+
+# x.update(y)
+# print(x)
+
+
+
+# -------------- Remove element using remove()
+
+# x = {1, 2, 3, 4, 5}
+
+# x.remove(3)
+# print(x)
+
+
+
+# -------------- Remove element using discard()
+
+# x = {1, 2, 3, 4, 5}
+
+# x.discard(4)
+# print(x)
+
+
+
+# -------------- Remove random item
+
+# x = {1, 2, 3, 4, 5}
+
+# x.pop()
+# print(x)
+
+
+
+# -------------- Clear set
+
+# x = {1, 2, 3, 4, 5}
+
+# x.clear()
+# print(x)
+
+
+
+# -------------- Union Set operations
+
+# a = {1, 2, 3}
+# b = {3, 4, 5}
+
+# # print(a | b)
+# print(a.union(b))
+
+
+
+# -------------- Intersection Set operations
+
+# a = {1, 2, 3}
+# b = {3, 4, 5}
+
+# print(a & b)
+
+
+
+# -------------- Difference Set operations
+
+# a = {1, 2, 3}
+# b = {3, 4, 5}
+
+# print(a - b)
+
+
+
+# -------------- Symmetric Difference Set operations
+
+# a = {1, 2, 3}
+# b = {3, 4, 5}
+
+# print(a ^ b)
+
+
+
+# -------------- Subset Set operations
+
+# a = {1, 2}
+# b = {1, 2, 3}
+
+# print(a <= b)
+
+
+
+# -------------- Superset Set operations
+
+# a = {1, 2, 3}
+# b = {1, 2}
+
+# print(a >= b)
