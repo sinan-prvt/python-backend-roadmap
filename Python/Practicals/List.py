f14e742e9dbf5a7109f6a33a43d5577b2b72b8cb
sinan-prvt <mohamedsinan9400@gmail.com>
2026-04-27 10:04:14 +0530

    Added list Practiical Questions

---
+++ Python/Practicals/List.py
@@ -0,0 +1,134 @@
+##========================================= List
+
+
+
+# -------------- Access element
+
+# x = [1, 2, 3, 4, 5]
+# print(x[0])
+
+# print(x[-1])      # Negative indexing
+
+# print(len(x))     # Length of list
+
+
+
+# -------------- Append element
+
+# num = [1, 2, 3]
+# num.append(4)
+# print(num)
+
+
+
+# -------------- Insert element
+
+# num = [1, 2, 3]
+# num.insert(1, 6)
+# print(num)
+
+
+
+# -------------- Extend list
+
+# num = [1, 2, 3]
+# num.extend([4, 5, 6])
+# print(num)
+
+
+
+# -------------- Remove by value
+
+# num = [1, 2, 3, 4, 5]
+# num.remove(3)
+# print(num)
+
+
+
+# -------------- Remove last element
+
+# num = [1, 2, 3, 4, 5]
+# num.pop()
+# print(num)
+
+
+
+# -------------- Remove by index
+
+# num = [1, 2, 3, 4, 5]
+# num.pop(2)
+# print(num)
+
+
+
+# -------------- Clear list
+
+# num = [1, 2, 3, 4, 5]
+# num.clear()
+# print(num)
+
+
+
+# -------------- Change element
+
+# num = [1, 2, 3, 4, 5]
+# num[1] = 7
+# print(num)
+
+
+
+# -------------- Slice list
+
+# num = [1, 2, 3, 4, 5]
+# print(num[1:4])
+
+
+
+# -------------- Reverse list
+
+# num = [1, 2, 3, 4, 5]
+# num.reverse()
+# print(num)
+
+
+
+# -------------- Sort list
+
+# num = [5, 2, 3, 1, 4]
+# num.sort()
+# print(num)
+
+
+
+# -------------- Sort list in reverse
+
+# num = [5, 2, 3, 1, 4]
+# num.sort(reverse=True)
+# print(num)
+
+
+
+# ------------- Sorted list without changing original
+
+# num = [5, 2, 3, 1, 4]
+# sorted_num = sorted(num)
+# print(num)
+# print(sorted_num)
+
+
+
+# -------------- Iterate list 
+
+# for i in [1, 2, 3]:
+#     print(i)
+
+
+
+# -------------- Iterate list with index
+
+# num = [1, 2, 3]
+
+# for i in range(len(num)):
+#     print(i, num[i])
+
+
