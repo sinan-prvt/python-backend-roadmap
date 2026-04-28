6c158526098191c687097cc1b8182e8988563b8e
sinan-prvt <mohamedsinan9400@gmail.com>
2026-04-28 14:47:30 +0530

    Added Moduled practical questions

---
+++ Python/Practicals/Module.py
@@ -0,0 +1,43 @@
+##========================================= Modules
+
+
+
+# -------------- math Module
+
+# import math
+
+# print(math.sqrt(16))
+
+
+
+# -------------- random Module
+
+# import random
+
+# print(random.randint(1, 10))
+
+
+
+# -------------- datetime Module
+
+# from datetime import datetime
+
+# print(datetime.now())
+
+
+
+# -------------- os Module
+
+# import os
+
+# print(os.getcwd())
+
+
+
+# -------------- sys Module
+
+# import sys
+
+# print(sys.version)
+
+
