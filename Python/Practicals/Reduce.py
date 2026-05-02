8ffde2afdb3a08434d5c34c0b0a6d0e47a032646
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-02 16:07:05 +0530

    Added reduce() Practical questions

---
+++ Python/Practicals/Reduce.py
@@ -0,0 +1,41 @@
+##========================================= reduce() function
+
+
+
+# -------------- Sum of numbers
+
+# from functools import reduce
+
+# nums = [1, 2, 3, 4, 5]
+
+# result = reduce(lambda x, y: x + y, nums)
+
+# print(result)
+
+
+
+
+# -------------- Multiply numbers
+
+# from functools import reduce
+
+# nums = [1, 2, 3, 4, 5]
+
+# result = reduce(lambda x, y: x * y, nums)
+
+# print(result)
+
+
+
+# -------------- With Initial Value 
+
+# from functools import reduce
+
+# nums = [1, 2, 3, 4, 5]
+
+# result = reduce(lambda x, y: x + y, nums, 10)
+
+# print(result)
+
+
+
