75fcc45f20f0541af624c018e8ce65e21c52239f
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-02 16:22:51 +0530

    Added zip() Practical questions

---
+++ Python/Practicals/Zip().py
@@ -0,0 +1,49 @@
+##========================================= zip() Function
+
+
+
+# -------------- Combine two lists
+
+# names = ["sinan", "ahmet", "ayse"]
+# ages = [19, 20, 21]
+
+# result = zip(names, ages)
+
+# print(list(result))
+
+
+
+
+# -------------- Looping with zip()
+
+# names = ["sinan", "ahmet", "ayse"]
+# ages = [19, 20, 21]
+
+# for name, age in zip(names, ages):
+#     print(name, age)
+
+
+
+
+# -------------- Unziping
+
+# pairs = [("sinan", 19), ("ahmet", 20), ("ayse", 21)]
+
+# names, ages = zip(*pairs)
+
+# print(names)
+# print(ages)
+
+
+
+
+# -------------- With Loop + Index 
+
+# names = ["sinan", "ahmet", "ayse"]
+# ages = [19, 20, 21]
+
+# for i, (n, a) in enumerate(zip(names, ages)):
+#     print(i, n, a)
+
+
+
