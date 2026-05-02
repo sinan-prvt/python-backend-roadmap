ba113e87767b56382a99abcdf2d1dfd6d85a31c3
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-02 16:37:22 +0530

    Added enumerte() Practical questions

---
+++ Python/Practicals/Enumerate().py
@@ -0,0 +1,34 @@
+##========================================= enumerate() Function
+
+
+
+# -------------- Basic Usage
+
+# names = ["sinan", "ahmet", "ayse"]
+
+# for i, name in enumerate(names):
+#     print(i, name)
+
+
+
+
+# -------------- Start index 
+
+# names = ["sinan", "ahmet", "ayse"]
+
+# for i, name in enumerate(names, start=101):
+#     print(i, name)
+
+
+
+
+# -------------- enumerate + zip
+
+# names = ["sinan", "ahmet", "ayse"]
+# ages = [19, 20, 21]
+
+# for i, (name, age) in enumerate(zip(names, ages)):
+#     print(i, name, age)
+
+
+
