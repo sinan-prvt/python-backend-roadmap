f42a92903924f1e0b6ad65d9488f3215d56f3d46
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-07 20:24:16 +0530

    Added Oops Encapsulation Practical Question

---
+++ Python/Practicals/Oops/Encapsulation.py
@@ -0,0 +1,70 @@
+##========================================= Encapsulation
+
+
+
+# -------------- Encapsulation using methods
+
+# class Bank:
+
+#     def __init__(self):
+#         self.__balance = 40
+
+#     def deposit(self, amount):
+#         self.__balance += amount
+    
+#     def get_balance(self):
+#         return self.__balance
+
+# b = Bank()
+
+# b.deposit(100)
+
+# print(b.get_balance())
+
+
+
+
+
+## ================================ Types of Encapsulation
+
+
+## ------------------ Public Encapsulation
+
+# class Student:
+
+#     def __init__(self):
+#         self.name = "Sinan"
+
+# s = Student()
+
+# print(s.name)
+
+
+
+
+## ------------------ Protected Encapsulation
+
+# class Student:
+
+#     def __init__(self):
+#         self._age = 19
+
+# s = Student()
+
+# print(s._age)
+
+
+
+
+## ------------------ Private Encapsulation
+
+class Bank:
+
+    def __init__(self):
+        self.__balance = 40
+    
+    def get_balance(self):
+        return self.__balance
+    
+b = Bank()
+print(b.get_balance())
