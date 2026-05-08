7ad3709064b1879811723cf16267763f73b7cc4a
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-08 11:46:48 +0530

    Added Django Setup Process

---
+++ Django/Practicals/Setup/SetUp.py
@@ -0,0 +1,89 @@
+# =================================== Setup Django Step-bt-Step
+
+
+
+# --------------- Step 1: Check if Python is installed
+
+# python --version
+
+
+
+# --------------- Step 2: Create Project Folder
+
+# mkdir ProjectName
+
+
+
+# --------------- Step 3: Create Virtual Environment
+
+# python -m venv env
+
+
+
+# --------------- Step 4: Activate Virtual Environment
+
+# env\Scripts\activate
+
+
+
+# --------------- Step 5: Update pip
+
+# python -m pip install --upgrade pip
+
+
+
+# --------------- Step 6: Install Django
+
+# python install django
+
+
+
+# --------------- Step 7: Verify Django Installation
+
+# django-admin --version
+
+
+
+# --------------- Step 8: Create Django Project
+
+# django-admin startproject ProjectName
+
+
+
+# -------------- Step 9: Run Server
+
+# python manage.py runserver
+
+
+
+# --------------- Step 10: Create Django App
+
+# python manage.py startapp AppName
+
+
+
+# -------------- Step 11: Run Migrations
+
+# python manage.py makemigrations
+# python manage.py migrate
+
+
+
+# --------------- Step 12: Create Superuser
+
+# python manage.py createsuperuser
+
+
+
+# --------------- Step 13: Create requirements.txt file
+
+# pip freeze > requirements.txt
+
+
+
+# --------------- Step 14: Install requirements from requirements.txt file
+
+# pip install -r requirements.txt
+
+
+
