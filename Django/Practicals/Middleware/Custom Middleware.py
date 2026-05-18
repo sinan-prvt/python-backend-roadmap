08d385754f987eec3cdf952ff13f9eebf0670325
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-18 17:58:04 +0530

    Added custom middleware Practical

---
+++ Django/Practicals/Middleware/Custom Middleware.py
@@ -0,0 +1,32 @@
+# =================================== Middleware
+
+
+
+# --------------- Basic Middleware Example
+
+
+## == middleware.py
+
+# def simple_middleware(get_response):
+
+#     def middleware(request):
+#         print("Before view")
+#         response = get_response(request)
+#         print("After view")
+
+#         return response
+
+#     return middleware
+
+
+
+## settings.py
+
+# MIDDLEWARE = [
+#     'app.middleware.simple_middleware',
+# ]
+
+
+
+
+
