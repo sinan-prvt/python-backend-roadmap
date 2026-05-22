dfe160a1a04533749255ee14d92c7fcfb4d9431b
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-22 20:45:53 +0530

    Added Unique Key Practical

---
+++ SQL/Practicals/Keys/UniqueKey.py
@@ -0,0 +1,31 @@
+# =================================== Unique Key
+
+
+
+# --------------- Create Table with Unique Key
+
+
+# CREATE TABLE Employees (
+#     id SERIAL PRIMARY KEY,
+#     name VARCHAR(25) UNIQUE,
+#     email VARCHAR(50) UNIQUE
+# );
+
+
+
+# --------------- Insert Data into Table
+
+# INSERT INTO Employees (name, email)
+# VALUES ('Alice', 'alice@example.com'),
+#        ('Bob', 'bob@example.com');
+
+
+
+# --------------- Fetch Data Using Unique Key   
+
+# SELECT * FROM Employees
+# WHERE email = 'alice@example.com';
+
+
+
+
