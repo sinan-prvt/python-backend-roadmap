ad5b5accf62ec7e6fa003314e4c088dbc2fa5995
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-23 21:37:04 +0530

    Added Concatenation Operators Practicals

---
+++ SQL/Practicals/Operators/Concatenation.py
@@ -0,0 +1,22 @@
+# =================================== Concatenation Operators
+
+
+
+# --------------- Concatenation Operator (||)
+
+# SELECT first_name || ' ' || last_name AS full_name FROM Employees;    
+
+# -- Concatenate first and last names with a space in between
+
+
+
+
+# --------------- CONCAT Function
+
+# SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM Employees;    
+
+# -- Concatenate first and last names with a space in between using CONCAT function
+
+
+
+
