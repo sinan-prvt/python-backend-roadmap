fe35d860e6143df6a41f2e15fb2ae0b6f19566ee
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-23 21:19:00 +0530

    Added Arithemetic Operators Practicals

---
+++ SQL/Practicals/Operators/Arithemetic.py
@@ -0,0 +1,54 @@
+# =================================== Arthmetic Operators
+
+
+
+# --------------- Add Values
+
+# SELECT 10 + 5 AS Addition;           -- Output: 15
+
+
+
+
+# --------------- Subtract Values
+
+# SELECT 10 - 5 AS Subtraction;        -- Output: 5
+
+
+
+
+# --------------- Multiply Values
+
+# SELECT 10 * 5 AS Multiplication;     -- Output: 50
+
+
+
+
+# --------------- Divide Values
+
+# SELECT 10 / 5 AS Division;           -- Output: 2
+
+
+
+
+# --------------- Modulo (Remainder)
+
+# SELECT 10 % 3 AS Modulo;             -- Output: 1 (10 divided by 3 leaves a remainder of 1)
+
+
+
+
+# --------------- Exponentiation
+
+# SELECT 2 ^ 3 AS Exponentiation;     -- Output: 8 (2 raised to the power of 3 is 8)
+
+
+
+
+# --------------- Combine Arithmetic Operations
+
+# SELECT (10 + 5) * 2 AS Combined;     -- Output: 30 (First adds 10 and 5, then multiplies the result by 2)
+
+
+
+
+
