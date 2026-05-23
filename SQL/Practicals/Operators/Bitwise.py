ed66bd3880b05ad09ce94e4657e8a753a867e599
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-23 21:26:53 +0530

    Added Bitwise Operators Practicals

---
+++ SQL/Practicals/Operators/Bitwise.py
@@ -0,0 +1,41 @@
+# =================================== Bitwise Operators
+
+
+
+# --------------- Bitwise AND
+
+# SELECT 5 & 3 AS BitwiseAND;    -- Output: 1 (In binary: 5 is 101 and 3 is 011, so the result is 001 which is 1 in decimal)
+
+
+
+# --------------- Bitwise OR
+
+# SELECT 5 | 3 AS BitwiseOR;     -- Output: 7 (In binary: 5 is 101 and 3 is 011, so the result is 111 which is 7 in decimal)
+
+
+
+# --------------- Bitwise XOR
+
+# SELECT 5 ^ 3 AS BitwiseXOR;    -- Output: 6 (In binary: 5 is 101 and 3 is 011, so the result is 110 which is 6 in decimal)
+
+
+
+# --------------- Bitwise NOT
+
+# SELECT ~5 AS BitwiseNOT;        -- Output: -6 (In binary: 5 is 0000 0101, so the result is 1111 1010 which is -6 in decimal using two's complement)
+
+
+
+# --------------- Bitwise Shift Left
+
+# SELECT 5 << 1 AS ShiftLeft;    -- Output: 10 (In binary: 5 is 101, shifting left by 1 gives 1010 which is 10 in decimal)
+
+
+
+# --------------- Bitwise Shift Right
+
+# SELECT 5 >> 1 AS ShiftRight;   -- Output: 2 (In binary: 5 is 101, shifting right by 1 gives 10 which is 2 in decimal)
+
+
+
+
