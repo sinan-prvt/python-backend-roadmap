d0230ce44d8a4f4556f2e6508d2e69a5c8a9d3c5
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-23 21:32:20 +0530

    Added Pattern Matching Operators Practicals

---
+++ SQL/Practicals/Operators/PatternMatching.py
@@ -0,0 +1,44 @@
+# =================================== Pattern Matching Operators
+
+
+
+# --------------- LIKE Operator
+
+# SELECT * FROM Employees
+# WHERE name LIKE 'A%';    -- Retrieve employees whose names start with 'A'
+
+
+
+
+# --------------- NOT LIKE Operator
+
+# SELECT * FROM Employees
+# WHERE name NOT LIKE 'A%';    -- Retrieve employees whose names do not start with
+
+
+
+
+# -------------- ILIKE Operator (case-insensitive)
+
+# SELECT * FROM Employees
+# WHERE name ILIKE 'a%';    -- Retrieve employees whose names start with 'a' (case-insensitive)
+
+
+
+
+# -------------- NOT ILIKE Operator (case-insensitive)
+
+# SELECT * FROM Employees
+# WHERE name NOT ILIKE 'a%';    -- Retrieve employees whose names do not start with 'a' (case-insensitive)
+
+
+
+
+# ------------- WILD CARD CHARACTERS
+
+# SELECT * FROM Employees
+# WHERE name LIKE '_a%';    -- Retrieve employees whose names have 'a' as
+
+
+
+
