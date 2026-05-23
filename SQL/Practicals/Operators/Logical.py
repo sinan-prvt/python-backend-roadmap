ac229e4b692879de0fa86f25536ed52e6ca9fc80
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-23 21:24:56 +0530

    Added Logical Operators Practicals

---
+++ SQL/Practicals/Operators/Logical.py
@@ -0,0 +1,28 @@
+# =================================== Logical Operators
+
+
+
+# --------------- AND Operator
+
+# SELECT * FROM Employees
+# WHERE department = 'HR' AND age > 30;    -- Retrieve employees who work in HR and are older than 30
+
+
+
+
+# --------------- OR Operator
+
+# SELECT * FROM Employees
+# WHERE department = 'HR' OR department = 'IT';    -- Retrieve employees who work
+
+
+
+
+# --------------- NOT Operator
+
+# SELECT * FROM Employees
+# WHERE NOT department = 'HR';    -- Retrieve employees who do not work in HR
+
+
+
+
