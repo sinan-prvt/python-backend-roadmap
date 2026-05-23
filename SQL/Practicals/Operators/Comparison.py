ee83f6026ce34f49e9ed4695477183931021bc43
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-23 21:23:02 +0530

    Added Comparison Operators Practicals

---
+++ SQL/Practicals/Operators/Comparison.py
@@ -0,0 +1,53 @@
+# =================================== Comparison Operators
+
+
+
+# --------------- Equal To
+
+# SELECT * FROM Employees
+# WHERE department = 'HR';          -- Retrieve employees who work in the HR department
+
+
+
+
+# --------------- Not Equal To
+
+# SELECT * FROM Employees
+# WHERE department != 'HR';         -- Retrieve employees who do not work in the HR department
+
+
+
+
+# -------------- Greater Than
+
+# SELECT * FROM Employees
+# WHERE age > 30;                   -- Retrieve employees who are older than 30
+
+
+
+
+# -------------- Less Than
+
+# SELECT * FROM Employees
+# WHERE age < 30;                   -- Retrieve employees who are younger than 30
+
+
+
+
+# -------------- Greater Than or Equal To
+
+# SELECT * FROM Employees
+# WHERE age >= 30;                  -- Retrieve employees who are 30 years old or older
+
+
+
+
+# -------------- Less Than or Equal To
+
+# SELECT * FROM Employees
+# WHERE age <= 30;                  -- Retrieve employees who are 30 years old or younger
+
+
+
+
+
