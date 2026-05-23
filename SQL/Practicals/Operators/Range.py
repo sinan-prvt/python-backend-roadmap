33573c40761b020ed5ff1ad30662355034f71c46
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-23 21:28:03 +0530

    Added Range Operators Practicals

---
+++ SQL/Practicals/Operators/Range.py
@@ -0,0 +1,21 @@
+# =================================== Range Operators
+
+
+
+# --------------- Range Operator (BETWEEN)
+
+# SELECT * FROM Employees
+# WHERE age BETWEEN 30 AND 40;    -- Retrieve employees whose age is between
+
+
+
+
+
+# --------------- NOT BETWEEN Operator
+
+# SELECT * FROM Employees
+# WHERE age NOT BETWEEN 30 AND 40;    -- Retrieve employees whose age is not between 30 and 40
+
+
+
+
